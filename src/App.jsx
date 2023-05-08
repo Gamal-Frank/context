2c2dbbff9ef4d7ddf9b44a13207fbd6ca295a2fd
frank <gamalgamer10@yahoo.com>
2023-05-08 14:26:44 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-
+import { useCallback, useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState({ id: null, name: "", sprites: "" });
+
+  console.log(pokemon);
+  const search = useCallback(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}/`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((poke) => {
+        setPokemon({
+          ...pokemon,
+          id: poke.id,
+          sprites: poke.sprites.front_default,
+        });
+      });
+  }, [pokemon]);
 
   return (
     <div>
+      <button onClick={search}>search</button>
       
+      <input
+        type="text"
+        onChange={(e) => {
+          setPokemon({ ...pokemon, name: e.target.value.toLowerCase() });
+        }}
+      />
+      <img src={pokemon.sprites} alt="" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
